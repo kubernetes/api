kubernetes/kubernetes

Unversioned types should not use ambiguous go-int
All external types that are not int64 are now marked as int32,
including
IntOrString. Prober is now int32 (43 years should be enough of an initial
probe time for anyone).

Did not change the metadata fields for now.

pkg/apis/extensions/validation/validation.go
@@ -251,7 +251,7 @@ func getIntOrPercentValue(intOrStringValue intstr.IntOrString) int {
 	if isPercent {
 		return value
 	}
-	return intOrStringValue.IntVal
+	return intOrStringValue.IntValue()
 }
 
 func IsNotMoreThan100Percent(intOrStringValue intstr.IntOrString, fieldName string) errs.ValidationErrorList {
@@ -524,7 +524,7 @@ func validateIngressBackend(backend *extensions.IngressBackend) errs.ValidationE
 		if !utilvalidation.IsValidPortName(backend.ServicePort.StrVal) {
 			allErrs = append(allErrs, errs.NewFieldInvalid("servicePort", backend.ServicePort.StrVal, apivalidation.PortNameErrorMsg))
 		}
-	} else if !utilvalidation.IsValidPortNum(backend.ServicePort.IntVal) {
+	} else if !utilvalidation.IsValidPortNum(backend.ServicePort.IntValue()) {
 		allErrs = append(allErrs, errs.NewFieldInvalid("servicePort", backend.ServicePort, apivalidation.PortRangeErrorMsg))
 	}
 	return allErrs

kubernetes/api

Kubernetes-commit: 1d592e4c285660e8d45d179d5081c85d2b770286

pkg/apis/extensions/validation/validation.go
@@ -249,7 +249,7 @@ func getIntOrPercentValue(intOrStringValue intstr.IntOrString) int {
 	if isPercent {
 		return value
 	}
-	return intOrStringValue.IntVal
+	return intOrStringValue.IntValue()
 }
 
 func IsNotMoreThan100Percent(intOrStringValue intstr.IntOrString, fieldName string) validation.ErrorList {
@@ -525,7 +525,7 @@ func validateIngressBackend(backend *extensions.IngressBackend) validation.Error
 		if !validation.IsValidPortName(backend.ServicePort.StrVal) {
 			allErrs = append(allErrs, validation.NewInvalidError("servicePort", backend.ServicePort.StrVal, apivalidation.PortNameErrorMsg))
 		}
-	} else if !validation.IsValidPortNum(backend.ServicePort.IntVal) {
+	} else if !validation.IsValidPortNum(backend.ServicePort.IntValue()) {
 		allErrs = append(allErrs, validation.NewInvalidError("servicePort", backend.ServicePort, apivalidation.PortRangeErrorMsg))
 	}
 	return allErrs