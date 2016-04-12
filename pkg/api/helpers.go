sdminonne/kubernetes

Adding nodeports services to quota

pkg/api/helpers.go
@@ -140,6 +140,7 @@ var standardQuotaResources = sets.NewString(
 	string(ResourceReplicationControllers),
 	string(ResourceSecrets),
 	string(ResourcePersistentVolumeClaims),
+	string(ResourceNodePorts),
 )
 
 // IsStandardQuotaResourceName returns true if the resource is known to
@@ -176,6 +177,7 @@ var integerResources = sets.NewString(
 	string(ResourceReplicationControllers),
 	string(ResourceSecrets),
 	string(ResourcePersistentVolumeClaims),
+	string(ResourceNodePorts),
 )
 
 // IsIntegerResourceName returns true if the resource is measured in integer values

kubernetes/api

Kubernetes-commit: 15b7577454f50954c5a60f230b24a969dc596809

pkg/api/helpers.go
@@ -152,6 +152,7 @@ var standardQuotaResources = sets.NewString(
 	string(ResourceSecrets),
 	string(ResourcePersistentVolumeClaims),
 	string(ResourceConfigMaps),
+	string(ResourceServicesNodePorts),
 )
 
 // IsStandardQuotaResourceName returns true if the resource is known to
@@ -190,6 +191,7 @@ var integerResources = sets.NewString(
 	string(ResourceSecrets),
 	string(ResourceConfigMaps),
 	string(ResourcePersistentVolumeClaims),
+	string(ResourceServicesNodePorts),
 )
 
 // IsIntegerResourceName returns true if the resource is measured in integer values