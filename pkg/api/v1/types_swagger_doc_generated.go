jayunit100/kubernetes

[scheduling] Auto-generated file updates from moving node affinity from annotations to api fields. #35518

pkg/api/v1/types_swagger_doc_generated.go
@@ -1297,6 +1297,7 @@ var map_PodSpec = map[string]string{
 	"imagePullSecrets":              "ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec. If specified, these secrets will be passed to individual puller implementations for them to use. For example, in the case of docker, only DockerConfig type secrets are honored. More info: http://kubernetes.io/docs/user-guide/images#specifying-imagepullsecrets-on-a-pod",
 	"hostname":                      "Specifies the hostname of the Pod If not specified, the pod's hostname will be set to a system-defined value.",
 	"subdomain":                     "If specified, the fully qualified Pod hostname will be \"<hostname>.<subdomain>.<pod namespace>.svc.<cluster domain>\". If not specified, the pod will not have a domainname at all.",
+	"affinity":                      "If specified, the pod's scheduling preferences",
 }
 
 func (PodSpec) SwaggerDoc() map[string]string {

pkg/apis/extensions/v1beta1/types.generated.go
@@ -22148,7 +22148,7 @@ func (x codecSelfer1234) decSliceDeployment(v *[]Deployment, d *codec1978.Decode
 
 			yyrg1854 := len(yyv1854) > 0
 			yyv21854 := yyv1854
-			yyrl1854, yyrt1854 = z.DecInferLen(yyl1854, z.DecBasicHandle().MaxInitLen, 856)
+			yyrl1854, yyrt1854 = z.DecInferLen(yyl1854, z.DecBasicHandle().MaxInitLen, 864)
 			if yyrt1854 {
 				if yyrl1854 <= cap(yyv1854) {
 					yyv1854 = yyv1854[:yyrl1854]
@@ -22264,7 +22264,7 @@ func (x codecSelfer1234) decSliceDaemonSet(v *[]DaemonSet, d *codec1978.Decoder)
 
 			yyrg1860 := len(yyv1860) > 0
 			yyv21860 := yyv1860
-			yyrl1860, yyrt1860 = z.DecInferLen(yyl1860, z.DecBasicHandle().MaxInitLen, 752)
+			yyrl1860, yyrt1860 = z.DecInferLen(yyl1860, z.DecBasicHandle().MaxInitLen, 760)
 			if yyrt1860 {
 				if yyrl1860 <= cap(yyv1860) {
 					yyv1860 = yyv1860[:yyrl1860]
@@ -22496,7 +22496,7 @@ func (x codecSelfer1234) decSliceJob(v *[]Job, d *codec1978.Decoder) {
 
 			yyrg1872 := len(yyv1872) > 0
 			yyv21872 := yyv1872
-			yyrl1872, yyrt1872 = z.DecInferLen(yyl1872, z.DecBasicHandle().MaxInitLen, 824)
+			yyrl1872, yyrt1872 = z.DecInferLen(yyl1872, z.DecBasicHandle().MaxInitLen, 832)
 			if yyrt1872 {
 				if yyrl1872 <= cap(yyv1872) {
 					yyv1872 = yyv1872[:yyrl1872]
@@ -23192,7 +23192,7 @@ func (x codecSelfer1234) decSliceReplicaSet(v *[]ReplicaSet, d *codec1978.Decode
 
 			yyrg1908 := len(yyv1908) > 0
 			yyv21908 := yyv1908
-			yyrl1908, yyrt1908 = z.DecInferLen(yyl1908, z.DecBasicHandle().MaxInitLen, 800)
+			yyrl1908, yyrt1908 = z.DecInferLen(yyl1908, z.DecBasicHandle().MaxInitLen, 808)
 			if yyrt1908 {
 				if yyrl1908 <= cap(yyv1908) {
 					yyv1908 = yyv1908[:yyrl1908]

kubernetes/api

Kubernetes-commit: 11c577f09245c2d1825ffcf6f77fae4a98f665a1

pkg/api/v1/types_swagger_doc_generated.go
@@ -1274,6 +1274,7 @@ var map_PodSpec = map[string]string{
 	"imagePullSecrets":              "ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec. If specified, these secrets will be passed to individual puller implementations for them to use. For example, in the case of docker, only DockerConfig type secrets are honored. More info: http://kubernetes.io/docs/user-guide/images#specifying-imagepullsecrets-on-a-pod",
 	"hostname":                      "Specifies the hostname of the Pod If not specified, the pod's hostname will be set to a system-defined value.",
 	"subdomain":                     "If specified, the fully qualified Pod hostname will be \"<hostname>.<subdomain>.<pod namespace>.svc.<cluster domain>\". If not specified, the pod will not have a domainname at all.",
+	"affinity":                      "If specified, the pod's scheduling constraints",
 }
 
 func (PodSpec) SwaggerDoc() map[string]string {

pkg/apis/extensions/v1beta1/types.generated.go
@@ -21849,7 +21849,7 @@ func (x codecSelfer1234) decSliceDeployment(v *[]Deployment, d *codec1978.Decode
 
 			yyrg1828 := len(yyv1828) > 0
 			yyv21828 := yyv1828
-			yyrl1828, yyrt1828 = z.DecInferLen(yyl1828, z.DecBasicHandle().MaxInitLen, 856)
+			yyrl1828, yyrt1828 = z.DecInferLen(yyl1828, z.DecBasicHandle().MaxInitLen, 864)
 			if yyrt1828 {
 				if yyrl1828 <= cap(yyv1828) {
 					yyv1828 = yyv1828[:yyrl1828]
@@ -21965,7 +21965,7 @@ func (x codecSelfer1234) decSliceDaemonSet(v *[]DaemonSet, d *codec1978.Decoder)
 
 			yyrg1834 := len(yyv1834) > 0
 			yyv21834 := yyv1834
-			yyrl1834, yyrt1834 = z.DecInferLen(yyl1834, z.DecBasicHandle().MaxInitLen, 752)
+			yyrl1834, yyrt1834 = z.DecInferLen(yyl1834, z.DecBasicHandle().MaxInitLen, 760)
 			if yyrt1834 {
 				if yyrl1834 <= cap(yyv1834) {
 					yyv1834 = yyv1834[:yyrl1834]
@@ -22197,7 +22197,7 @@ func (x codecSelfer1234) decSliceJob(v *[]Job, d *codec1978.Decoder) {
 
 			yyrg1846 := len(yyv1846) > 0
 			yyv21846 := yyv1846
-			yyrl1846, yyrt1846 = z.DecInferLen(yyl1846, z.DecBasicHandle().MaxInitLen, 824)
+			yyrl1846, yyrt1846 = z.DecInferLen(yyl1846, z.DecBasicHandle().MaxInitLen, 832)
 			if yyrt1846 {
 				if yyrl1846 <= cap(yyv1846) {
 					yyv1846 = yyv1846[:yyrl1846]
@@ -22893,7 +22893,7 @@ func (x codecSelfer1234) decSliceReplicaSet(v *[]ReplicaSet, d *codec1978.Decode
 
 			yyrg1882 := len(yyv1882) > 0
 			yyv21882 := yyv1882
-			yyrl1882, yyrt1882 = z.DecInferLen(yyl1882, z.DecBasicHandle().MaxInitLen, 800)
+			yyrl1882, yyrt1882 = z.DecInferLen(yyl1882, z.DecBasicHandle().MaxInitLen, 808)
 			if yyrt1882 {
 				if yyrl1882 <= cap(yyv1882) {
 					yyv1882 = yyv1882[:yyrl1882]